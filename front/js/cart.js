e540f47229b61c89d0fa36322aa8f9ffa5453a44
ClementH9 <henot.clement@gmail.com>
2023-04-12 09:16:11 +0200

    session mentor 12/04

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,17 +1,31 @@
-// Appel de l'API pour récupérer produits
-const params = new URL (document.location).searchParams;
-const id = params.get("id");
+// Récupération des produits dans le Local Storage
+let productsInLocalStorage = JSON.parse(localStorage.getItem("cart"));
+
+console.log(productsInLocalStorage);
+/*
+
 // Affichage des produits de la page panier
 function displayCardProducts();
 // Pour changer la quantité donnée d'un produit
 function changeQuantity(    
-    if(quantity = 0){
-        deleteProduct();
-    }else{
-        updateQuantity();
-    });
+    if (quantity = 0) {
+        
+    deleteProduct();
+} else {
+
+    updateQuantity();
+});
 // Suppression d'un produit du panier
-function deleteProduct();
+function deleteProduct(){
+
+};
 // Changer la quantité de produit dans le panier
-function updateQuantity();
+function updateQuantity(){
+    
+};
 // Gestion du formulaire (REGEX)
+let textRegex = new RegExp("^[^.?!:;,/\\/_-]([. '-]?[a-zA-Zàâäéèêëïîôöùûüç])+[^.?!:;,/\\/_-]$");
+let addressRegex = new RegExp("^[^.?!:;,/\\/_-]([, .:;'-]?[0-9a-zA-Zàâäéèêëïîôöùûüç])+[^.?!:;,/\\/_-]$");
+let emailRegex = new RegExp("^[^. ?!:;,/\\/_-]([._-]?[a-z0-9])+[^.?!: ;,/\\/_-][@][a-z0-9]+[.][a-z][a-z]+$");
+
+*/
--- front/js/index.js
+++ front/js/index.js
@@ -1,12 +1,10 @@
 // Récupération des canapés depuis le fichier JSON
-async function products()
-{
+async function products() {
     const reponse = await fetch('http://localhost:3000/api/products');
     return await reponse.json();
 }
 
-async function display()
-{
+async function display() {
     const canape = await products();
 
     for (let i = 0; i < canape.length; i++) {
@@ -17,7 +15,7 @@ async function display()
         const canapeElement = document.createElement("article");
         //Création des balises
         const linkElement = document.createElement("a");
-        linkElement.href="./product.html?id="+canape[i]._id;
+        linkElement.href = "./product.html?id=" + canape[i]._id;
         const imageElement = document.createElement("img");
         imageElement.src = canape[i].imageUrl;
         imageElement.alt = canape[i].altTxt;
@@ -27,7 +25,7 @@ async function display()
         const descriptionElement = document.createElement("p");
         descriptionElement.classList.add("productDescription");
         descriptionElement.innerText = canape[i].description;
-       
+
         // Rattacher la balise "a" (lien) à la section items
         sectionItems.appendChild(linkElement);
         // Rattacher la balise article à la section items
