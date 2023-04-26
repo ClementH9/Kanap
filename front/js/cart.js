20845147df7d80f9ff9e456f5071b16cf535d413
ClementH9 <henot.clement@gmail.com>
2023-04-26 20:53:47 +0200

    mentor 26/04 + prix quantité

--- front/js/cart.js
+++ front/js/cart.js
@@ -25,11 +25,19 @@ fetch(`http://localhost:3000/api/products`)
             let dataProductId = data[elData]._id ;
             if (cartProductId == dataProductId) {
 
-                const sectionCart = document.getElementById("cart__items") ;
+                let productColor = contentCart[elCart].productColor;
+
+                const sectionCart = document.getElementById("cart__items") ; // pointe vers l'id cart__items
+
+                const productArticle = document.createElement('article');
+                productArticle.setAttribute("class", "cart__item");
+                productArticle.setAttribute("data-id", dataProductId);
+                productArticle.setAttribute("data-color", productColor);
+                sectionCart.appendChild(productArticle);
 
                 let newProductImg = document.createElement('div');
                 newProductImg.setAttribute("class", "cart__item__img");
-                sectionCart.appendChild(newProductImg);
+                productArticle.appendChild(newProductImg);
 
                 let productImg = data[elData].imageUrl ;
                 const newProductImgTag = document.createElement('img');
@@ -37,42 +45,78 @@ fetch(`http://localhost:3000/api/products`)
                 newProductImgTag.setAttribute("alt", data[elData].altTxt);
                 newProductImg.appendChild(newProductImgTag);
                 
+                let productContent = document.createElement('div');
+                productContent.setAttribute("class", "cart__item__content");
+                productArticle.appendChild(productContent);
+                let productDescription = document.createElement('div');
+                productDescription.setAttribute("class", "cart__item__content__description");
+                productContent.appendChild(productDescription);
+
                 let productName = data[elData].name ;
                 const titleProduct = document.createElement("h2") ;
                 titleProduct.innerText = productName ;
-                sectionCart.appendChild(titleProduct) ;
+                productDescription.appendChild(titleProduct);
 
-                let productColor = contentCart[elCart].productColor ;
                 const colorProduct = document.createElement("p") ;
                 colorProduct.innerText = "Couleur : " + productColor ;
-                sectionCart.appendChild(colorProduct) ;
-
-                let productQuantity = contentCart[elCart].productQuantity ;
-                const quantityProduct = document.createElement("p") ;
-                quantityProduct.innerText = "Quantité : " + productQuantity ; 
-                sectionCart.appendChild(quantityProduct) ;
+                productDescription.appendChild(colorProduct) ;
 
                 let productPrice = data[elData].price ;
                 const priceProduct = document.createElement("p") ;
-                priceProduct.innerText = "Prix : " + (productPrice * productQuantity) + " €";
-                sectionCart.appendChild(priceProduct) ;
-                
+                priceProduct.innerText = "Prix unitaire : " + productPrice + " €";
+                productDescription.appendChild(priceProduct) ;
 
+                let productSettings = document.createElement('div');
+                productSettings.setAttribute("class", "cart__item__content__settings");
+                productContent.appendChild(productSettings);
+                let productSettingsQuantity = document.createElement('div');
+                productSettingsQuantity.setAttribute("class", "cart__item__content__settings__quantity");
+                productSettings.appendChild(productSettingsQuantity);
+                let productQuantity = contentCart[elCart].productQuantity ;
+                const quantityProduct = document.createElement("p") ;
+                quantityProduct.innerText = "Qté : "; 
+                productSettingsQuantity.appendChild(quantityProduct) ;
+                let productSettingsQuantityInput = document.createElement('input');
+                productSettingsQuantityInput.setAttribute("type", "number");
+                productSettingsQuantityInput.setAttribute("class", "itemQuantity");
+                productSettingsQuantityInput.setAttribute("name", "itemQuantity");
+                productSettingsQuantityInput.setAttribute("min", "1");
+                productSettingsQuantityInput.setAttribute("max", "100");
+                productSettingsQuantityInput.setAttribute("value", productQuantity);
+                productSettingsQuantity.appendChild(productSettingsQuantityInput);
+
+                let deleteProduct = document.createElement('div');
+                deleteProduct.setAttribute("class", "cart__item__content__settings__delete");
+                productSettings.appendChild(deleteProduct);
+                let deleteProductParagraphe = document.createElement('p');
+                deleteProductParagraphe.setAttribute ("class", "deleteItem");
+                deleteProductParagraphe.innerText = "Supprimer";
+                deleteProduct.appendChild(deleteProductParagraphe);
             }
-            /* const sectionCart = document.getElementById("cart__items");
-            const cartArticle = document.createElement("article");
-            document.querySelector("cart__items").appendChild(sectionCart);
-            const cartImg = document.createElement("div");
-            document.querySelector("cart__item").append(cartArticle);
-            const cartImg = document.createElement("div");
-            document.querySelector("cart__item__img").append(cartImg); */
             console.log(cartProductId)
         }
     }
 })
 
+/* Supprimer : event listener, fonction à créer pour MAJ*/
 
-    
+document.getElementById("totalQuantity").innerText=totalProductsQuantity(); //parseInt et créer fonction qui calcule la quantité
+document.getElementById("totalPrice").innerText=totalProductsPrice(); //parseInt et créer fonction qui calcule le prix - eventListener pour vérifier click ou événement qui change l'input MAJ le prix et Qté
+
+function totalProductsQuantity(){
+    totalQuantity += parseInt(productSettingsQuantity);
+    console.log("Total quantité panier",totalQuantity);
+    document.getElementById("totalQuantity").innerText = totalQuantity;
+}
+
+function totalProductsPrice (){
+    totalProductPricePanier = productQuantity * productPrice;
+    console.log(totalProductPricePanier);
+    // Calcul du prix total du panier
+    totalPrice += totalProductPricePanier;
+    console.log("Total prix panier",totalPrice);
+    document.getElementById("totalPrice").innerText = totalPrice; 
+    }
 
 /*
 // Affichage des produits de la page panier
