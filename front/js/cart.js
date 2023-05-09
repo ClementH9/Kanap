5a2f91ad23b1b362d88a746b33859bce6185a4e0
ClementH9 <henot.clement@gmail.com>
2023-05-09 16:10:59 +0200

    POST

--- front/js/cart.js
+++ front/js/cart.js
@@ -306,7 +306,12 @@ form.addEventListener("submit", function(e){
     console.log("form");
     if (validFirstName(form.firstName) && validLastName(form.lastName) && validAddress(form.address) && validCity(form.city) && validEmail(form.email)){
         form.submit();
-        let idCommand = strRandom();
+        fetch("http://localhost:3000/api/products/order", options)
+        .then(response => response.json())
+            .then(data => {
+                document.location.href =`confirmation.html?id=${data.orderId}`;
+                localStorage.clear();
+        })
         console.log(idCommand);
         window.location.href = "./confirmation.html?id=" + idCommand;
     }
